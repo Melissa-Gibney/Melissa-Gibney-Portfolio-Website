68925eb2e03dfb0f3d1c4998e47966ad7cc2a217
Melissa-Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-08-26 13:33:47 -0400

    Update Home.tsx
    Changed italics to bold in the purpose statement.

--- portfolio-website/src/Pages/Home.tsx
+++ portfolio-website/src/Pages/Home.tsx
@@ -17,7 +17,7 @@ function Home() {
             <div className='Body'>
                 {/* <img className="ProfilePic" src={"/project_images/general/Professional_Headshot.jpg"} width="200px" height="fit-content" alt=''></img> */}
                 <div className='Section' style={{marginTop: "100px", marginBottom: "100px"}}>
-                    <h1>Melissa Gibney is a UX engineer studying at Carnegie Mellon University. She is enthralled by audio software and AI and has a passion for creating <em>clean</em>, <em>playful</em> applications.</h1>
+                    <h1>Melissa Gibney is a UX engineer studying at Carnegie Mellon University. She is enthralled by audio software and AI and has a passion for creating <b>clean</b>, <b>playful</b> applications.</h1>
                 </div>
                 <List title="Highlighted Projects">
                     <PicoCTFCapstoneProjectCard/>
