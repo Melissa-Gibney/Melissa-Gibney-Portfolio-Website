8d5ef342f78ac46964f099981cc60029af7585f4
Melissa-Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-08-28 13:47:04 -0400

    Adjusted Footer Color

--- portfolio-website/src/Components/General/Footer.tsx
+++ portfolio-website/src/Components/General/Footer.tsx
@@ -4,21 +4,10 @@ interface FooterProps {
   pageName: "Home" | "Project" | "About";
 }
 
-const Footer: React.FC<FooterProps> = ({ pageName }) => {
-
-  var shadowColor = "rgba(92, 218, 126, 0.7)";
-
-  if(pageName == "Project")
-  {
-    shadowColor = "rgba(136, 92, 218, 0.7)";
-  }
-  else if(pageName == "About")
-  {
-    shadowColor = "rgba(92, 163, 218, 0.7)";
-  }
+const Footer: React.FC<FooterProps> = ({ }) => {
 
   return (
-    <div className="Footer" style={{boxShadow: "0px -10px 10px " + shadowColor}}>
+    <div className="Footer">
         <div className='FooterContent'>
             <div className="FooterColumn">
                 <h2>Footer Left</h2>
