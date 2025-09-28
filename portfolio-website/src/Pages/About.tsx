2b80c5397ca948272b0f7a7294d9e1c6986b4dba
Melissa-Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-09-28 14:12:47 -0400

    Update About.tsx
    Added more info about my hobbies.

--- portfolio-website/src/Pages/About.tsx
+++ portfolio-website/src/Pages/About.tsx
@@ -7,28 +7,32 @@ function About() {
     	<div className="PageContainer">
         	<Header pageName={'About'}/>
        		<div className='PageBody'>
-            	<div className='Contents'>
-					<div className='Section'>
-						<svg className="SVG" viewBox="0 0 850 850" style={{marginRight: "10px"}}>
-							<image href={'/Professional_Headshot_Square.svg'} width="100%" height="100%" preserveAspectRatio='xMidYMid meet'/>
-						</svg>
-						<p style={{marginLeft: "10px"}}>Text here hihkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkhjkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjhkjh</p>
-					</div>
-                	{/* <svg viewBox='0 0 100 100' xmlns="http://www.w3.org/2000/svg" href='/Professional_Headshot.svg' preserveAspectRatio="xMidYMid meet"/> */}
-            	</div>
-        	</div>
-        	<div className="BubbleContainer">
-            	<div style={{"--i":21} as React.CSSProperties}/>
-            	<div style={{"--i":10} as React.CSSProperties}/>
-            	<div style={{"--i":49} as React.CSSProperties}/>
-            	<div style={{"--i":35} as React.CSSProperties}/>
-            	<div style={{"--i":80} as React.CSSProperties}/>
-            	<div style={{"--i":63} as React.CSSProperties}/>
-            	<div style={{"--i":28} as React.CSSProperties}/>
-            	<div style={{"--i":55} as React.CSSProperties}/>
-            	<div style={{"--i":41} as React.CSSProperties}/>
-            	<div style={{"--i":35} as React.CSSProperties}/>
-            	<div style={{"--i":25} as React.CSSProperties}/>
+            	<div className='Section'>
+					<svg className="SVG" viewBox="0 0 850 850" style={{marginRight: "10px"}}>
+						<image href={'/Professional_Headshot_Square.svg'} width="100%" height="100%" preserveAspectRatio='xMidYMid meet'/>
+					</svg>
+					<p style={{marginLeft: "10px"}}>
+						Hello! I'm Melissa, and in my free time I love combining art and technology to create beautiful, interactive experiences.
+						A few of my hobbies include 3D modeling and printing, game development, and pixel art.
+					</p>
+				</div>
+				<div className="Section">
+					<p>
+						I picked up my 3D modeling and game development hobbies in early 2021 and have been experimenting in those mediums ever since!
+						My favorite 3D games tend to have very stylized graphics that require specific placement of topology and UV maps, so I have spent my time learning to emulate these styles.
+						I especially love character modeling, as it is incredibly rewarding to conceptualize a character and bring it to life with textures and rigging.
+					</p>
+				</div>
+				<div className="Section">
+					<svg className="SVG" viewBox="0 0 850 850" style={{marginRight: "10px"}}>
+						<image href={'/Professional_Headshot_Square.svg'} width="100%" height="100%" preserveAspectRatio='xMidYMid meet'/>
+					</svg>
+					<p>
+						In addition to character and asset modeling, I have also dabbled in 3D printing.
+						My favorite 3D printing project was a little pumpkin that I made for a competition while pursuing my undergraduate degrees.
+						The render of the pumpkin won the 3D modeling competition, and I now use the 3D prints as some fun Halloween decor!
+					</p>
+				</div>
         	</div>
         	<Footer pageName={'About'}/>
     	</div>
