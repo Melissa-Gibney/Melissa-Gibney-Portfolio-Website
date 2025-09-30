5e643d98a8045c23deee61b63249f3d8edbfb4a6
Melissa-Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-09-29 20:46:24 -0400

    Update About.tsx
    Undid the previous address change.

--- portfolio-website/src/Pages/About.tsx
+++ portfolio-website/src/Pages/About.tsx
@@ -9,7 +9,7 @@ function About() {
 				<Header pageName={'About'}/>
 				<div className='Section' style={{ justifyItems: "center", marginTop: "270px", marginBottom: "300px", marginInline: "10%"}}>
 					<svg className="SVG" viewBox="0 0 850 850" style={{marginRight: "10px"}}>
-						<image href={'./Professional_Headshot_Square.svg'} width="100%" height="100%" preserveAspectRatio='xMidYMid meet'/>
+						<image href={'../../public/Professional_Headshot_Square.svg'} width="100%" height="100%" preserveAspectRatio='xMidYMid meet'/>
 					</svg>
 					<p style={{fontSize: "2em", marginLeft: "10px", width: "fill"}}>
 						Hello! I'm Melissa, and in my free time I love combining art and technology to create beautiful, interactive experiences.
