e368a1c495d6c30abd6c8836e803ee8173adfad6
Melissa-Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-10-02 20:56:11 -0400

    Added Project page back in
    Adding the project page back into the site.

--- portfolio-website/src/Pages/Projects.tsx
+++ portfolio-website/src/Pages/Projects.tsx
@@ -1,54 +1,54 @@
-// import './Page.css';
-// import Header from '../Components/General/Header';
-// import Footer from '../Components/General/Footer';
-// // import Background from '../Components/General/Background.js';
-// import List from '../Components/General/List';
-// import RoboticDrumMachineCard from '../Components/Project_Cards/RoboticDrumMachineCard';
-// import PicoCTFCapstoneProjectCard from '../Components/Project_Cards/PicoCTFCapstoneProjectCard';
-// import AICharacterChatbotCard from '../Components/Project_Cards/AICharacterChatbotCard';
-// import FluteSynthesizerCard from '../Components/Project_Cards/FluteSynthesizerCard';
-// import CharacterCreatorCard from '../Components/Project_Cards/CharacterCreatorCard';
-// import GiantEagleAccessibilityDesignCard from '../Components/Project_Cards/GiantEagleAccessibilityDesignCard';
-// import BumbleMeetCard from '../Components/Project_Cards/BumbleMeetCard';
-// // import { Canvas } from '@react-three/fiber';
+import './Page.css';
+import Header from '../Components/General/Header';
+import Footer from '../Components/General/Footer';
+// import Background from '../Components/General/Background.js';
+import List from '../Components/General/List';
+import RoboticDrumMachineCard from '../Components/Project_Cards/RoboticDrumMachineCard';
+import PicoCTFCapstoneProjectCard from '../Components/Project_Cards/PicoCTFCapstoneProjectCard';
+import AICharacterChatbotCard from '../Components/Project_Cards/AICharacterChatbotCard';
+import FluteSynthesizerCard from '../Components/Project_Cards/FluteSynthesizerCard';
+import CharacterCreatorCard from '../Components/Project_Cards/CharacterCreatorCard';
+import GiantEagleAccessibilityDesignCard from '../Components/Project_Cards/GiantEagleAccessibilityDesignCard';
+import BumbleMeetCard from '../Components/Project_Cards/BumbleMeetCard';
+// import { Canvas } from '@react-three/fiber';
 
-// import '../Scripts/MouseTracking.ts'
+import '../Scripts/MouseTracking.ts'
 
-// function Projects() {
-//     return (
-//         <div className='PageContainer'>
-//             <div className='HeroContainer'>
-//                 <Header pageName={'Project'}/>
-//                 <div className='Section' style={{ width: "fit-content", justifyItems: "center", marginTop: "150px", marginBottom: "50px", marginInline: "10%"}}>
-//                     <p style={{fontSize: "2em", textAlign: "center"}}>
-//                         <b>Projects</b>
-//                     </p>
-//                 </div>
-//                 <div className='Section' style={{ justifyItems: "center", marginTop: "50px", marginBottom: "250px", marginInline: "10%"}}>
-//                     <p style={{fontSize: "2em"}}>
-//                         These projects demonstrate my <b>techical</b> and <b>creative</b> capabilities.
-//                         I have worked in many different industries, from audio engineering to cybersecurity, and throughout my education and career, I have maintained my passion for technology and creativity.
-//                         I am continuing work on some of these projects, and I am excited to share more about them in the future!
-//                     </p>
-//                 </div>
-//             </div>
-//             <div className="PageBody">
-//                 <div className='Contents'>
-//                     <List title = "">
-//                         <PicoCTFCapstoneProjectCard/>
-//                         <CharacterCreatorCard/>
-//                         <AICharacterChatbotCard/>
-//                         <FluteSynthesizerCard/>
-//                         <RoboticDrumMachineCard/>
-//                         <GiantEagleAccessibilityDesignCard/>
-//                         <BumbleMeetCard/>
-//                     </List>
-//                     <div style={{width: "100%", height: "100px", content: ""}}/>
-//                 </div>
-//             </div>
-//             <Footer pageName={'Project'}/>
-//         </div>
-//     );
-// }
+function Projects() {
+    return (
+        <div className='PageContainer'>
+            <div className='HeroContainer'>
+                <Header pageName={'Project'}/>
+                <div className='Section' style={{ width: "fit-content", justifyItems: "center", marginTop: "150px", marginBottom: "50px", marginInline: "10%"}}>
+                    <p style={{fontSize: "2em", textAlign: "center"}}>
+                        <b>Projects</b>
+                    </p>
+                </div>
+                <div className='Section' style={{ justifyItems: "center", marginTop: "50px", marginBottom: "250px", marginInline: "10%"}}>
+                    <p style={{fontSize: "2em"}}>
+                        These projects demonstrate my <b>techical</b> and <b>creative</b> capabilities.
+                        I have worked in many different industries, from audio engineering to cybersecurity, and throughout my education and career, I have maintained my passion for technology and creativity.
+                        I am continuing work on some of these projects, and I am excited to share more about them in the future!
+                    </p>
+                </div>
+            </div>
+            <div className="PageBody">
+                <div className='Contents'>
+                    <List title = "">
+                        <PicoCTFCapstoneProjectCard/>
+                        <CharacterCreatorCard/>
+                        <AICharacterChatbotCard/>
+                        <FluteSynthesizerCard/>
+                        <RoboticDrumMachineCard/>
+                        <GiantEagleAccessibilityDesignCard/>
+                        <BumbleMeetCard/>
+                    </List>
+                    <div style={{width: "100%", height: "100px", content: ""}}/>
+                </div>
+            </div>
+            <Footer pageName={'Project'}/>
+        </div>
+    );
+}
 
-// export default Projects;
+export default Projects;
