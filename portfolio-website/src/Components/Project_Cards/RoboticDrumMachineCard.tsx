b5f134d25d418bd3e1f8815abbb54336ad52e774
Melissa Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-11-11 22:19:41 -0500

    Updated RoboticDrumMachine thumbnail and fully removed the project-images folder from public.

--- portfolio-website/src/Components/Project_Cards/RoboticDrumMachineCard.tsx
+++ portfolio-website/src/Components/Project_Cards/RoboticDrumMachineCard.tsx
@@ -1,13 +1,13 @@
 import ProjectCard from "../General/ProjectCard";
 import Tag from "../General/Tag";
+import thumbnail from "../../assets/project_images/robotic_drum_machine/Senior-Design-Enclosure.jpg";
 
 const RoboticDrumMachineCard: React.FC<{}> = () => {
     const title = "Robotic Drum Machine"
-    const imageRef = "project_images/robotic_drum_machine/Senior-Design-Enclosure.jpg"
     const link = "/projects/robotic-drum-machine"
     const description = "A drum machine built using an Arduino Mega, solenoids, and 3D modeling.";
     return (
-        <ProjectCard title={title} imageRef={imageRef} link={link} description={description} imagePadding={"0px"}>
+        <ProjectCard title={title} imageRef={thumbnail} link={link} description={description} imagePadding={"0px"}>
             <Tag name={"Microcontrollers"} color={"rgb(157, 120, 164)"}/>
             <Tag name={"CAD"} color={"rgb(94, 166, 108)"}/>
             <Tag name={"Research"} color={"rgb(94, 140, 166)"}/>
