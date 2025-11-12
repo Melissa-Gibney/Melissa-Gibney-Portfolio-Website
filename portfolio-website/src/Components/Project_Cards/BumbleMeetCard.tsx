cc7c774ae2893b0483de5bfeaba7c5f514e163f6
Melissa Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-11-11 21:45:59 -0500

    Update BumbleMeet images, Remove unneeded aicharacterchatbot images from public folder.

--- portfolio-website/src/Components/Project_Cards/BumbleMeetCard.tsx
+++ portfolio-website/src/Components/Project_Cards/BumbleMeetCard.tsx
@@ -1,13 +1,14 @@
 import ProjectCard from "../General/ProjectCard";
 import Tag from "../General/Tag";
+import thumbnail from "../../assets/project_images/bumble_meet/bumble_meet_thumbnail.svg";
 
 const BumbleMeetCard: React.FC<{}> = () => {
     const title = "Bumble Meet"
-    const imageRef = "project_images/bumble_meet/bumble_meet_thumbnail.svg"
+    // const imageRef = "project_images/bumble_meet/bumble_meet_thumbnail.svg"
     const link = "/projects/bumble-meet"
     const description = "A design for a new Bumble feature that allows users to connect in-person using location services.";
     return (
-        <ProjectCard title={title} imageRef={imageRef} link={link} description={description} imagePadding={"0px"}>
+        <ProjectCard title={title} imageRef={thumbnail} link={link} description={description} imagePadding={"0px"}>
             <Tag name={"Design"} color={"rgb(94, 166, 108)"}/>
             <Tag name={"Figma"} color={"rgb(94, 166, 108)"}/>
         </ProjectCard>
