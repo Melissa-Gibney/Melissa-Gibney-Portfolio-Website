3f9812670913e0b94ed412ca53b1a3e0160a52c2
Melissa Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-11-11 22:01:01 -0500

    Updated FluteSynthesizerCard images and moved them to the assets folder.

--- portfolio-website/src/Components/Project_Cards/FluteSynthesizerCard.tsx
+++ portfolio-website/src/Components/Project_Cards/FluteSynthesizerCard.tsx
@@ -1,13 +1,13 @@
 import ProjectCard from "../General/ProjectCard";
 import Tag from "../General/Tag";
+import thumbnail from "../../assets/project_images/flute_synthesizer/flute_synthesizer_thumbnail.svg";
 
 const FluteSynthesizerCard: React.FC<{}> = () => {
     const title = "Flute Synthesizer";
-    const imageRef = "project_images/flute_synthesizer/flute_synthesizer_thumbnail.svg";
     const link = "/projects/flute-synthesizer";
     const description = "A flute synthesizer coded based upon research by Perry R. Cook.";
     return (
-        <ProjectCard title={title} imageRef={imageRef} link={link} description={description} imagePadding={"10px"}>
+        <ProjectCard title={title} imageRef={thumbnail} link={link} description={description} imagePadding={"10px"}>
             <Tag name={"Max/MSP"} color={"rgb(157, 120, 164)"}/>
             <Tag name={"Design"} color={"rgb(94, 166, 108)"}/>
         </ProjectCard>
