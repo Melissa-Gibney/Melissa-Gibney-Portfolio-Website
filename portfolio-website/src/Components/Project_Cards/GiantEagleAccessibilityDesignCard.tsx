40ab417c746ed7eb1d10a4cde1c09c088d71fdb4
Melissa Gibney <120681456+Melissa-Gibney@users.noreply.github.com>
2025-11-11 22:11:35 -0500

    Updated GiantEagleAccessibility image location and card

--- portfolio-website/src/Components/Project_Cards/GiantEagleAccessibilityDesignCard.tsx
+++ portfolio-website/src/Components/Project_Cards/GiantEagleAccessibilityDesignCard.tsx
@@ -1,13 +1,14 @@
 import Card from "../General/ProjectCard";
 import Tag from "../General/Tag";
+import thumbnail from "../../assets/project_images/giant_eagle_accessibility_design/thumbnail.svg";
 
 const GiantEagleAccessibilityDesignCard: React.FC<{}> = () => {
     const title = "Grocery Store Accessibility Design";
-    const imageRef = "project_images/giant_eagle_accessibility_design/thumbnail.svg";
+    // const imageRef = "project_images/giant_eagle_accessibility_design/thumbnail.svg";
     const link = "/projects/giant-eagle-accessibility-design";
     const description = "A design using AR to assist users with navigating through their shopping experience.";
     return (
-        <Card title={title} imageRef={imageRef} link={link} description={description} imagePadding={"10px"}>
+        <Card title={title} imageRef={thumbnail} link={link} description={description} imagePadding={"10px"}>
             <Tag name={"Research"} color={"rgb(94, 140, 166)"}/>
             <Tag name={"Design"} color={"rgb(94, 166, 108)"}/>
             <Tag name={"Figma"} color={"rgb(94, 166, 108)"}/>
